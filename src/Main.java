e567daf50946ca311a04a5d3ab27fc26396aecb3
Слава Сойнов <v.soynov@gmail.com>
2022-10-25 13:31:27 +0600

    Init

--- src/Main.java
+++ src/Main.java
@@ -6,69 +6,54 @@ public class Main {
         System.out.println("Введите выражение по шаблону a + b, a - b, a * b или a / b.");
         System.out.println("a и b арабские или римские числа от 1 до 10:");
         Scanner sc = new Scanner(System.in);
-        String expression=sc.nextLine();
+        String input=sc.nextLine();
         sc.close();
-        System.out.println(expression + " = " + calc(expression));
+        System.out.println(input + " = " + calc(input));
     }
-    public static String calc(String expression) throws IOException {
-        int result=0, num1, num2;
-        boolean isRoman = false;
-        String resultString = "";
-                String[] splitedString = expression.split(" "); //раскладываем на элементы (число, мат.оператор, число)
-        if ((splitedString.length == 3) && isMathOperator(splitedString[1])) { //Проверяем что параметров 3 и в середине мат.оператор
-                if (isValidArabic(splitedString[0]) && isValidArabic(splitedString[2])) {//Проверяем что первый и третий параметры - арабские числа
-                    num1 = Integer.parseInt(splitedString[0]);
-                    num2 = Integer.parseInt(splitedString[2]);
-                    char math = splitedString[1].charAt(0);
-                    result=calculate(num1,num2,math);
-                } else if (isValidRoman(splitedString[0]) && isValidRoman(splitedString[2])) { //Проверяем что первый и третий параметры - римские числа
-                    isRoman = true;
-                    num1 = romanToArabic(splitedString[0]);
-                    num2 = romanToArabic(splitedString[2]);
-                    char math = splitedString[1].charAt(0);
-                    result = calculate(num1, num2, math);
-                } else if ((isValidArabic(splitedString[0]) && isValidRoman(splitedString[2])) || (isValidArabic(splitedString[2]) && isValidRoman(splitedString[0]))) {
-                    System.out.println("Неправильно введены данные: смешаны арабские и римские числа"); //Exсeption!
+    public static String calc(String input) throws IOException {
+        int num1, num2;
+                String[] splittedString = input.split(" "); //раскладываем на элементы (число, мат.оператор, число)
+        if ((splittedString.length == 3) && isMathOperator(splittedString[1])) { //Проверяем что параметров 3 и в середине мат.оператор
+                if (isValidArabic(splittedString[0]) && isValidArabic(splittedString[2])) {//Проверяем что первый и третий параметры - арабские числа
+                    num1 = Integer.parseInt(splittedString[0]);
+                    num2 = Integer.parseInt(splittedString[2]);
+                    char math = splittedString[1].charAt(0);
+                    return Integer.toString(calculate(num1, num2, math));
+                } else if (isValidRoman(splittedString[0]) && isValidRoman(splittedString[2])) { //Проверяем что первый и третий параметры - римские числа
+                    num1 = romanToArabic(splittedString[0]);
+                    num2 = romanToArabic(splittedString[2]);
+                    char math = splittedString[1].charAt(0);
+                    return arabicToRoman(calculate(num1, num2, math));
+                } else if ((isValidArabic(splittedString[0]) && isValidRoman(splittedString[2])) || (isValidArabic(splittedString[2]) && isValidRoman(splittedString[0]))) {
+                    System.out.println("Неправильно введены данные: смешаны арабские и римские числа");
                     throw new IOException();
                 }
                 else {
-                    System.out.println("Неправильно введены данные: a и b не арабские/римские числа"); //Exсeption!
+                    System.out.println("Неправильно введены данные: a и b не арабские/римские числа в диапазоне 1-10");
                     throw new IOException();
                 }
         }
         else {
-            System.out.println("Неправильно введены данные: нет двух чисел и мат.оператора записаных через пробел"); //Exсeption!
+            System.out.println("Неправильно введены данные: нет двух чисел и мат.оператора записаных через пробел");
             throw new IOException();
         }
-        if (isRoman) {
-            resultString = arabicToRoman(result);
-        }
-        else {
-            resultString = Integer.toString(result);
-        }
-        return resultString;
     }
-    static boolean isValidArabic(String str) {
+    static boolean isValidArabic(String str) { //проверка на число 1-10
         int size = str.length();
-        for (int i = 0; i < size; i++) {
-            if (!Character.isDigit(str.charAt(i))) {
-                return false;
-            }
-        }
-        return (0 < size) && (Integer.parseInt(str)>0) && (Integer.parseInt(str)<11);
+        return (0 < size) && str.matches("[1-9]|10");
     }
-    static boolean isValidRoman(String str) {
+    static boolean isValidRoman(String str) { //проверка на римское число I-X
         int size = str.length();
-        return (0 < size) && str.matches("^(X{0,1})(V?I{0,3}|I[VX])$");
+        return (0 < size) && str.matches("X|V?I{0,3}|I[VX]");
     }
-    static boolean isMathOperator(String str) throws IOException {
+    static boolean isMathOperator(String str) throws IOException { //проверка правильного мат. оператора
         if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
             return true;
         }
         System.out.println("Введена неправильная мат. операция");
         throw new IOException();
     }
-    static int calculate(int num1, int num2, char math) {
+    static int calculate(int num1, int num2, char math) throws IOException {
         if (math=='+') {
             return num1+num2;
         }
@@ -81,7 +66,7 @@ public class Main {
         if (math=='/') {
             return num1/num2;
         }
-    return -1;
+        throw new IOException();
     }
     static int romanToArabic(String str) {
         int size = str.length();
@@ -103,12 +88,11 @@ public class Main {
                     romanNum += 1;
                 }
             }
-
         }
         return romanNum;
     }
     static String arabicToRoman(int number) throws IOException {
-        if (number > 100 || number < 1) {
+        if (number < 1) {
             System.out.println("Результат не является римским числом");
             throw new IOException();
         }
@@ -129,16 +113,16 @@ public class Main {
             } else if (number >= 10) {
                 result +="X";
                 number -= 10;
-            } else if (number >= 9) {
+            } else if (number == 9) {
                 result +="IX";
                 number -= 9;
             } else if (number >= 5) {
                 result +="V";
                 number -= 5;
-            } else if (number >= 4) {
+            } else if (number == 4) {
                 result +="IV";
                 number -= 4;
-            } else if (number >= 1) {
+            } else {
                 result +="I";
                 number -= 1;
             }
